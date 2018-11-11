391bea4111b1e377ff39d71227e9d4a823aa8d84
Adam Guinea <adamguinea22@gmail.com>
2018-11-11 23:07:19 +0000

    removed cached

--- src/index.js
+++ src/index.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+import {API_KEY} from '../keys'
 
-import App from './components/app';
-import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('hiking')
+    }
+    
+    videoSearch(term){
+        YTSearch({key:API_KEY, term: term}, (videos) => {
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            })
+        })
+    }
+    
+    render(){
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        
+        return (
+            <div> 
+                <SearchBar onSearchTermChange={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo} />
+                <VideoList 
+                onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+                videos={this.state.videos} />
+            </div>
+        )
+    }
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+// take this component's HTML and put it in the DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,15 @@
 module.exports = {
-  entry: ['./src/index.js'],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
+  entry: [
+    './src/index.js'
+  ],
   module: {
-    loaders: [
-      {
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015', 'stage-1']
-        }
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-1']
       }
-    ]
+    }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -22,9 +17,9 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './',
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 1000
-    }
+    inline: true,
+    host: process.env.IP,
+    port: process.env.PORT,
+    public: process.env.C9_HOSTNAME
   }
 };
